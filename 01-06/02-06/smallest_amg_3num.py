c20c7d3fb2bcc9fcb2a22754ec613e0e5f470539
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2023-06-02 16:24:49 +0000

    while

--- 01-06/02-06/smallest_amg_3num.py
+++
@@ -1,9 +0,0 @@
-num1=int(input("enter any numbers:"))
-num2=int(input("enter any numbers:"))
-num3=int(input("enter any numbers:"))
-if(num1<num2 and num1<num3):
-    print(num1,"is a smallest")
-elif(num2<num1 and num2<num3):
-    print(num2,"is a smallest")    
-else:
-    print(num3,"is a smallest")
