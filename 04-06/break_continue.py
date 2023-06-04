b06f8c9608018eda57efbe5f79c9c5facde32213
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2023-06-04 16:30:50 +0000

    today practice pgm

---
+++ 04-06/break_continue.py
@@ -0,0 +1,12 @@
+a=9
+b=9
+while a<10:
+    print("a is less than 10")
+    if(b<4):
+        print("b is greater")
+        continue
+    print("nxt")
+    if(a==b):
+        break  
+    b=b+1
+print("koms")    
