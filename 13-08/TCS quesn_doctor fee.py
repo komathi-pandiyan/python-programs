d683821228fad485e866c979faa68fac80807ce2
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2023-08-13 15:50:40 +0000

    Doctor fee TCS program

---
+++ 13-08/TCS quesn_doctor fee.py
@@ -0,0 +1,19 @@
+age = []
+for i in range(2):
+    m = input()
+    if m == "":
+        break
+    elif int(m) in range(0,120):
+        age.append(int(m))
+    else:
+        print("INVALID INPUT")
+        exit()
+fees = 0
+for i in age:
+    if i < 17:
+        fees+=200
+    elif i <40:
+        fees+=400
+    else:
+        fees+=300
+print("Total Income {} INR".format(fees))
