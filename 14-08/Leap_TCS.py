5c38190299238af64c559de4e9172bec76103b98
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2023-08-14 17:04:42 +0000

    Leap year program

---
+++ 14-08/Leap_TCS.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the year you want to check if it is a leap year or not: "))
+if num % 4 == 0:
+    if num % 100 == 0:
+        if num % 400 == 0:
+            print("The year {} is a leap year".format(num))
+        else:
+            print("The year {} is Not a leap year".format(num))
+    else:
+        print("The year {} is a leap year".format(num))
+else:
+    print("The year {} is Not a leap year".format(num))
