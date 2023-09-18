91cfac15d787d61f888d1a36d16e1db0dcf9bba6
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2023-09-18 15:38:03 +0000

    TCS pgm

---
+++ 18-09/TCS.py
@@ -0,0 +1,14 @@
+trainee = [[],[],[],[]]
+for i in range(3):
+	for j in range(3):
+    		trainee[i].append(int(input()))
+    		if (trainee[i][-1]) not in range(1,101):
+        			print("invalid input")
+for i in range(3):
+	trainee[3].append((trainee[2][i]+trainee[1][i]+trainee[0][i])//3)
+maximum = max(trainee[3])
+for i in range(3):
+	if trainee[3][i] < 70 :
+    		print("Trainee {0} is unfit".format(i+1))
+	elif trainee[3][i] == maximum:
+    		print("Trainee Number: ",i+1)
