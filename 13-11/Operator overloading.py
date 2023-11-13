86ebd23a3d93dac5c3d9d3607a38f70e1841c524
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2023-11-13 15:27:22 +0000

    operator overloading

---
+++ 13-11/Operator overloading.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __lt__(self, other):
+        return self.age < other.age
+p1 = Person("Alice", 20)
+p2 = Person("Bob", 30)
+print(p1 < p2) 
+print(p2 < p1) 
