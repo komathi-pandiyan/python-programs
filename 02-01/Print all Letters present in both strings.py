da95974497e2ac59550c0da1d3d2c5e68119f12a
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2024-01-03 06:48:21 +0000

    Sum of All the Items in the Dictionary

--- 02-01/Print all Letters present in both strings.py
+++ 02-01/Print all Letters present in both strings.py
@@ -1,5 +1,5 @@
-s1=raw_input("Enter first string:")
-s2=raw_input("Enter second string:")
+s1=input("Enter first string:")
+s2=input("Enter second string:")
 a=list(set(s1)|set(s2))
 print("The letters are:")
 for i in a:
