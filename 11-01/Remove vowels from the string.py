33f7adf4c87e221f88f6fd0116ac17434d61a800
komathi-pandiyan <85017790+komathi-pandiyan@users.noreply.github.com>
2024-01-11 15:44:55 +0000

    Remove vowels from the string

---
+++ 11-01/Remove vowels from the string.py
@@ -0,0 +1,7 @@
+string = input("Enter a String : ") 
+result=''
+for i in string:  
+    if i in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):  
+        i = ''  
+    result += i 
+print("String after removing the vowels :",result)
